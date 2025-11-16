da6b7fefd0ab64e4bc1e9429d1a53a8023b98ca6
ssds <cby724828@gmail.com>
2025-11-16 16:12:06 +0800

    ooo

--- web_app.py
+++ web_app.py
@@ -11,12 +11,14 @@ from flask_cors import CORS  # 添加这行
 app = Flask(__name__)
 CORS(app)  # 添加这行
 #app.config.update(
- #   CELERY_BROKER_URL='redis://redis:6379/0',
+ # CELERY_BROKER_URL='redis://redis:6379/0',
   #  CELERY_RESULT_BACKEND='redis://redis:6379/0'
 #)
 app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379/0',  # 本地Redis
-    CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+CELERY_BROKER_URL='redis://redis:6379/0',
+  CELERY_RESULT_BACKEND='redis://redis:6379/0'
+    #CELERY_BROKER_URL='redis://localhost:6379/0',  # 本地Redis
+    #CELERY_RESULT_BACKEND='redis://localhost:6379/0'
 )
 
 # 共享卷的路径，对应 docker-compose.yml
